3db59aec47a860666b2aef35e4f305458a3a0199
Lewis Thorley <lewis.thorley@launchb.co.uk>
2020-02-25 07:05:02 +0000

    Tutorial starts here

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import {View, Text} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>Tutorial starts here...</Text>
+    </View>
+  );
+};
+export default App;
